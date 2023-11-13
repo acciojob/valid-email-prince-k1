8bd268f01e551fc7d655d5d21c451a47d02b9f27
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-13 11:56:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function validEmail(str) {
   //your JS code here.
+	let arr = str.split('.');
+	let result = false;
+	let arr2 = arr[0].split('');
+	arr2.forEach((elem) => {
+		if(elem == '@'){
+			result = true;
+		}
+	})
+	if(result && arr2[0] != '@'){
+		return true;
+	}
+	return false;
 }
 
 // Do not change the code below.
